cbe07587abfbe81e3489e04a63346ba13ee39acf
C. Roest <c.roest@icloud.com>
2025-09-02 20:59:46 +0200

    Add multiple output classes

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ import numpy as np
 from data import make_tf_dataset
 from model import build_model
 
+in_shape = 1024, 1024, 3
+out_classes = {
+    0: "Background",
+    1: "Tool clasper",
+    2: "Tool wrist",
+    3: "Tool shaft",
+    4: "Suturing needle",
+    5: "Thread",
+    6: "Suction tool",
+    7: "Needle Holder",
+    8: "Clamps",
+    9: "Catheter",
+}
+
 
 if __name__ == "__main__":
-    in_shape = 1024, 1024, 3
-    m = build_model(in_shape)
-    m.summary()
-
-    dataset = make_tf_dataset('/mnt/d/code_data')
+    dataset = make_tf_dataset("/mnt/d/code_data", in_shape[:2])
     for i, (imgs, segs) in enumerate(dataset.take(5)):
         print(imgs.numpy().shape)
         print(segs.numpy().shape)
 
-        # Export a few samples to the workdir 
+        # Export a few samples to the workdir
         img = imgs.numpy().astype(np.uint8)
         seg = segs.numpy().astype(np.uint8) * 20  # Scale segmentation for visibility
         cv2.imwrite(f"sample_img_{i}.png", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
         cv2.imwrite(f"sample_seg_{i}.png", cv2.cvtColor(seg, cv2.COLOR_RGB2BGR))
-    
+
+    m = build_model(in_shape, len(out_classes))
+    m.summary()
+
+    sample_input = dataset.take(5).batch(5)
+    preds = m.predict(sample_input)  # Run a prediction to test the model
+
+    print(preds.shape)  # Should be (5, 1024, 1024, 3)
--- model.py
+++ model.py
@@ -3,7 +3,10 @@ from tensorflow.keras.layers import Input, Conv2D
 from tensorflow.keras.models import Model
 
 
-def build_model(input_shape: tuple):
+def build_model(
+    input_shape: tuple,
+    num_classes: int,
+) -> tf.keras.models.Model:
     """
     Basic model class that implements a very simple model to test the rest of the pipeline
 
@@ -14,11 +17,15 @@ def build_model(input_shape: tuple):
         tensorflow.keras.models.Model: Keras model.
     """
     i = Input(input_shape)
-    o = Conv2D(3, (3, 3), padding="same")(i)
+
+    # Add a layer that rescales input images to the [0, 1] range
+    i = tf.keras.layers.Rescaling(1.0 / 255)(i)
+
+    o = Conv2D(num_classes, (3, 3), padding="same")(i)
     return Model(i, o)
 
 
 if __name__ == "__main__":
     in_shape = 1024, 1024, 3
-    m = build_model(in_shape)
+    m = build_model(in_shape, 1)
     m.summary()
