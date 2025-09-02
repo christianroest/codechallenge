ba05e7f3e8684ccdd1fac8143bbbae5002b89780
C. Roest <c.roest@icloud.com>
2025-09-02 19:40:59 +0200

    Add overall data loading function

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 from os import path
 from glob import glob
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
@@ -33,12 +33,14 @@ def find_segmentation_paths(base_dir: str) -> List[str]:
         list: A list of file paths matching the specified extension within the directory structure.
     """
     # Find all segmentations in the directory and its subdirectories
-    matches = sorted(glob(path.join(base_dir, "**", "segmentation", f"*.png"), recursive=True))
+    matches = sorted(
+        glob(path.join(base_dir, "**", "segmentation", f"*.png"), recursive=True)
+    )
 
     return matches
 
 
-def read_images(image_paths: List[str]) -> np.ndarray:
+def read_images_to_numpy(image_paths: List[str]) -> np.ndarray:
     """
     Reads images from the provided file paths and stacks them into a single numpy array.
 
@@ -49,6 +51,50 @@ def read_images(image_paths: List[str]) -> np.ndarray:
         numpy.ndarray: A numpy array containing all the images stacked along a new axis.
     """
 
-    images = [cv2.imread(str(p)) for p in image_paths]
+    images = []
+    for i, p in enumerate(image_paths):
+        img = cv2.imread(p)
+        if img is None:
+            raise ValueError(f"Image at path {p} could not be read.")
+        images.append(img)
+        print(i, "/", len(image_paths))
     images = np.stack(images, axis=0)
     return images
+
+
+def load_dataset(base_dir: str, limit: int = 99999999) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Loads a dataset of images and their corresponding segmentations from subdirectories within a base directory.
+    The function searches for subdirectories matching the pattern "video_*/" inside the specified base directory.
+    For each subdirectory, it attempts to find image and segmentation files, reads them into NumPy arrays.
+
+    Args:
+        base_dir (str): The base directory containing subdirectories with image and segmentation data.
+        limit (int, optional): The maximum number of images and segmentations to load from each subdirectory, for testing.
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple containing two NumPy arrays:
+            - images: Array of loaded images.
+            - segmentations: Array of loaded segmentation masks.
+    """
+
+    # Look for subdirectories containing the data
+    subdirs = glob(path.join(base_dir, "video_*/"))
+
+    for subdir in subdirs:
+        print(f"Loading data from {subdir}...")
+        try:
+            image_paths = find_image_paths(subdir)[:limit]
+            segmentation_paths = find_segmentation_paths(subdir)[:limit]
+
+            images = read_images_to_numpy(image_paths)
+            segmentations = read_images_to_numpy(segmentation_paths)
+
+            print(
+                f"Loaded {images.shape[0]} images and {segmentations.shape[0]} segmentations from {subdir}."
+            )
+
+        except Exception as e:
+            print(f"Error loading data from {subdir}: {e}, skipping this directory.")
+            continue
+
+    return images, segmentations
